d453d966e13429f09e3bf9ad2470eb841ffa8fd1
Daniel <skyrimdaniel11@gmail.com>
2026-03-09 22:03:28 -0300

    feat: add tests to user

--- app/DTO/User/UpdateUserDTO.php
+++ app/DTO/User/UpdateUserDTO.php
@@ -12,7 +12,26 @@ class UpdateUserDTO extends AbstractDTO
         public readonly string $name,
         public readonly DocumentTypeEnum $document_type,
         public readonly string $document_number,
-        public readonly string $password,
+        public readonly null|string $password,
     ) {
+    }
+
+    public function toArray(): array
+    {
+        if ($this->password !== null) {
+            return [
+                'id' => $this->id,
+                'name' => $this->name,
+                'document_type' => $this->document_type,
+                'document_number' => $this->document_number,
+                'password' => $this->password,
+            ];
+        }
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'document_type' => $this->document_type,
+            'document_number' => $this->document_number,
+        ];
     }
 }
