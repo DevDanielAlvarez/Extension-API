5c0464a8d7a276ec58e81d850ef6ecb025037bb7
Daniel <skyrimdaniel11@gmail.com>
2026-03-11 08:00:00 -0300

    feat: add dto to create patient

---
+++ app/DTO/Patient/CreatePatientDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DTO\Patient;
+
+use Alvarez\ConcreteDto\AbstractDTO;
+use App\Enums\DocumentTypeEnum;
+use Carbon\Carbon;
+
+class CreatePatientDTO extends AbstractDTO
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly DocumentTypeEnum $document_type,
+        public readonly string $document_number,
+        public readonly Carbon $admission_date,
+        public readonly Carbon $birthday,
+        public readonly string $phone,
+        public readonly array $nursing_report
+    ) {
+    }
+}
