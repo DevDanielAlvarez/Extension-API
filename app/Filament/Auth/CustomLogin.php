aad8abb245f2a7cfdf52dc07528d865401a85a95
Daniel <skyrimdaniel11@gmail.com>
2026-03-13 09:42:44 -0300

    feat: add logic to login and lang

--- app/Filament/Auth/CustomLogin.php
+++ app/Filament/Auth/CustomLogin.php
@@ -3,11 +3,15 @@
 namespace App\Filament\Auth;
 
 use App\Enums\DocumentTypeEnum;
+use App\Models\User;
+use Filament\Auth\Http\Responses\LoginResponse;
 use Filament\Auth\Pages\Login;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
 
 class CustomLogin extends Login
 {
@@ -24,6 +28,29 @@ class CustomLogin extends Login
                     ->visible(fn(Get $get) => $get('document_type'))
                     ->translateLabel(),
                 TextInput::make('password')
+                    ->translateLabel()
             ]);
+    }
+
+    public function authenticate(): \Filament\Auth\Http\Responses\Contracts\LoginResponse|null
+    {
+        $data = $this->form->getState();
+        $user = User::where('document_type', $data['document_type'])
+            ->where('document_number', $data['document_number'])
+            ->first();
+
+        if (!$user) {
+            return null;
+        }
+
+        if (!Hash::check($data['password'], $user->password)) {
+            return null;
+        }
+
+        Filament::auth()->login($user);
+
+        session()->regenerate();
+
+        return app(LoginResponse::class);
     }
 }
