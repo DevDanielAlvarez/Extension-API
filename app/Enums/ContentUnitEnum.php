f34c78f13f4734a0655b3a21d0e66e8a7f69c0a6
daniel <skyrimdaniel11@gmail.com>
2026-05-08 11:38:00 -0300

    feat: add translate

--- app/Enums/ContentUnitEnum.php
+++ app/Enums/ContentUnitEnum.php
@@ -2,7 +2,10 @@
 
 namespace App\Enums;
 
-enum ContentUnitEnum: string
+use Filament\Support\Contracts\HasLabel;
+use Illuminate\Contracts\Support\Htmlable;
+
+enum ContentUnitEnum: string implements HasLabel
 {
     case MG = 'MG';
     case MCG = 'MCG';
@@ -10,4 +13,15 @@ enum ContentUnitEnum: string
     case ML = 'ML';
     case IU = 'IU';
     case UNIT = 'UNIT';
+
+    public function getLabel(): string | Htmlable | null{
+        return match ($this) {
+            self::MG => __('MG'),
+            self::MCG => __('MCG'),
+            self::G => __('G'),
+            self::ML => __('ML'),
+            self::IU => __('IU'),
+            self::UNIT => __('UNIT'),
+        };
+    }
 }
